dc61a1e50e3a6737dd3148e23d8cefb1548a4e0d
chloe hausser <chloerae018@gmail.com>
2023-05-26 13:08:11 -0400

    hello world

--- src/main.c
+++ src/main.c
@@ -1,20 +1,10 @@
 #include <stdio.h>
 
-#include "reader.h"
+
 
 int main(int argc, char *argv[]){
 
-    if(argc < 2){
-        printf("Please provide a .kcats file to run.");
-        return 0;
-    }
-
-    Reader reader;
-    initReader(&reader);
-    setFile(&reader, argv[1]);
-    readFile(&reader);
-    printf("%s\n", reader.content);
-    freeReader(&reader);
+    printf("Hello, World!\n");
 
     return 0;
 }
